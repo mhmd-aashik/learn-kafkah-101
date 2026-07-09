646c10d427019b029adb72e02486a04510b6bba6
Mohammed Aashik <aashikdevelop@gmail.com>
2026-07-10 03:11:39 +0530

    Implement Kafka event handling for order creation in payment service, adding payment processing logic with success and failure scenarios, and enhancing logging for better traceability.

--- apps/payment-service/src/payment-service.controller.ts
+++ apps/payment-service/src/payment-service.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { PaymentServiceService } from './payment-service.service';
+import { EventPattern, Payload } from '@nestjs/microservices';
+import type { OrderCreatedEvent } from '‎libs/shared/event.contracts';
 
 @Controller()
 export class PaymentServiceController {
+  private readonly logger = new Logger(PaymentServiceController.name);
+
   constructor(private readonly paymentServiceService: PaymentServiceService) {}
 
-  @Get()
-  getHello(): string {
-    return this.paymentServiceService.getHello();
+  @EventPattern('order.created')
+  async handleOrderCreated(@Payload() data: OrderCreatedEvent) {
+    this.logger.log(
+      `[Kafka Listener] Received 'order.created' event for Order: ${data.orderId}`,
+    );
+
+    // Process the payment simulation asynchronously
+    await this.paymentServiceService.processPayment(data);
   }
 }
--- apps/payment-service/src/payment-service.service.ts
+++ apps/payment-service/src/payment-service.service.ts
@@ -1,8 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import {
+  OrderCreatedEvent,
+  PaymentCompletedEvent,
+  PaymentFailedEvent,
+} from '‎libs/shared/event.contracts';
 
 @Injectable()
-export class PaymentServiceService {
-  getHello(): string {
-    return 'Hello World!';
+export class PaymentServiceService implements OnModuleInit {
+  private readonly logger = new Logger(PaymentServiceService.name);
+
+  constructor(
+    @Inject('KAFKA_CLIENT') private readonly kafkaClient: ClientKafka,
+  ) {}
+
+  async onModuleInit() {
+    this.logger.log('Connecting to Kafka broker as producer...');
+    await this.kafkaClient.connect();
+    this.logger.log('Kafka broker connection established.');
+  }
+
+  async processPayment(orderCreatedEvent: OrderCreatedEvent): Promise<void> {
+    const { orderId, userId, amount, courseId } = orderCreatedEvent;
+
+    this.logger.log(
+      `[Payment] Started processing payment for Order: ${orderId} (Amount: $${amount / 100})`,
+    );
+
+    await new Promise((resolve) => setTimeout(resolve, 2500));
+
+    const isSuccess = Math.random() > 0.2;
+
+    if (isSuccess) {
+      // 3. Prepare success event payload
+      const successPayload: PaymentCompletedEvent = {
+        orderId,
+        userId,
+        courseId,
+        amount,
+      };
+
+      this.logger.log(
+        `[Payment] Payment SUCCESS for Order ${orderId}. Emitting 'payment.completed'`,
+      );
+
+      this.kafkaClient.emit(
+        'payment.completed',
+        JSON.stringify(successPayload),
+      );
+    } else {
+      // 4. Prepare failure event payload
+      const failurePayload: PaymentFailedEvent = {
+        orderId,
+        userId,
+        courseId,
+        amount,
+        reason: 'INSUFFICIENT_FUNDS', // Fake reason
+      };
+
+      this.logger.warn(
+        `[Payment] Payment FAILED for Order ${orderId}. Emitting 'payment.failed'`,
+      );
+      this.kafkaClient.emit('payment.failed', JSON.stringify(failurePayload));
+    }
   }
 }
