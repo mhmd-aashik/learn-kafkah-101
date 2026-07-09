9cfb7013de839bcac57a6c70f66fbbeb2035e6a4
Mohammed Aashik <aashikdevelop@gmail.com>
2026-07-10 01:09:10 +0530

    Add Kafka client configuration to orders API service module, integrating environment variables for broker settings and enhancing microservices capabilities.

--- apps/orders-api-service/src/orders-api-service.module.ts
+++ apps/orders-api-service/src/orders-api-service.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { OrdersApiServiceController } from './orders-api-service.controller';
 import { OrdersApiServiceService } from './orders-api-service.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ClientsModule.registerAsync([
+      {
+        name: 'KAFKA_CLIENT',
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.KAFKA,
+          options: {
+            client: {
+              clientId: 'orders-api-producer',
+              brokers: [
+                configService.get<string>('KAFKA_BROKER', 'localhost:9092'),
+              ],
+            },
+            consumer: {
+              groupId: 'orders-api-producer-group',
+            },
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+  ],
   controllers: [OrdersApiServiceController],
   providers: [OrdersApiServiceService],
 })
