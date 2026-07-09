469bddb7def1b5113bef7bf7e8c527b793a22a31
Mohammed Aashik <aashikdevelop@gmail.com>
2026-07-10 02:25:19 +0530

    Integrate global validation and Kafka microservice support in orders API service, enhancing request handling and enabling asynchronous messaging capabilities.

--- apps/orders-api-service/src/main.ts
+++ apps/orders-api-service/src/main.ts
@@ -1,8 +1,45 @@
 import { NestFactory } from '@nestjs/core';
 import { OrdersApiServiceModule } from './orders-api-service.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersApiServiceModule);
-  await app.listen(process.env.port ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'orders-api-producer',
+        brokers: [process.env.KAFKA_BROKER || 'localhost:9092'],
+      },
+      consumer: {
+        groupId: 'orders-api-group',
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+
+  const port = process.env.PORT_ORDERS_API || 3000;
+  await app.listen(port);
+
+  console.log(
+    `\n=============================================================`,
+  );
+  console.log(`🚀 Orders API Service is running on http://localhost:${port}`);
+  console.log(
+    `   Connected to Kafka Broker: ${process.env.KAFKA_BROKER || 'localhost:9092'}`,
+  );
+  console.log(
+    `=============================================================\n`,
+  );
 }
 void bootstrap();
