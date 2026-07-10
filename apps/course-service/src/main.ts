154843f7b8217e5dfb0f8c0e66a8b43542f2849f
Mohammed Aashik <aashikdevelop@gmail.com>
2026-07-10 14:25:54 +0530

    Refactor course service to use Kafka microservice architecture, enabling asynchronous event handling and enhancing logging for service startup and broker connection details.

--- apps/course-service/src/main.ts
+++ apps/course-service/src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { CourseServiceModule } from './course-service.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(CourseServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    CourseServiceModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          clientId: 'course-service-consumer',
+          brokers: [process.env.KAFKA_BROKER || 'localhost:9092'],
+        },
+        consumer: {
+          groupId: 'course-service-group', // Distinct consumer group for course service
+        },
+      },
+    },
+  );
+
+  await app.listen();
+
+  console.log(
+    `\n=============================================================`,
+  );
+  console.log(`🚀 Course Service (Pure Kafka Consumer) is running...`);
+  console.log(
+    `   Listening on Kafka Broker: ${process.env.KAFKA_BROKER || 'localhost:9092'}`,
+  );
+  console.log(
+    `=============================================================\n`,
+  );
 }
-bootstrap();
+void bootstrap();
