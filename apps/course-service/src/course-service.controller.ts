eedb9f057fd3b140efd22124344ae9d1cda54924
Mohammed Aashik <aashikdevelop@gmail.com>
2026-07-10 14:24:53 +0530

    Add event handling for payment completion in course service, integrating Kafka listener to unlock courses and enhancing logging for better traceability.

--- apps/course-service/src/course-service.controller.ts
+++ apps/course-service/src/course-service.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { CourseServiceService } from './course-service.service';
+import { EventPattern, Payload } from '@nestjs/microservices';
+import type { PaymentCompletedEvent } from '‎libs/shared/event.contracts';
 
 @Controller()
 export class CourseServiceController {
+  private readonly logger = new Logger(CourseServiceController.name);
   constructor(private readonly courseServiceService: CourseServiceService) {}
 
-  @Get()
-  getHello(): string {
-    return this.courseServiceService.getHello();
+  @EventPattern('payment.completed')
+  async handlePaymentCompleted(@Payload() data: PaymentCompletedEvent) {
+    this.logger.log(
+      `[Kafka Listener] Received 'payment.completed' event for Order: ${data.orderId}`,
+    );
+
+    await this.courseServiceService.unlockCourse(data);
   }
 }
